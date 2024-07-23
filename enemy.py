98229ea7326e2af900d9449ddfd0f62e277a58cb
tjmontufar <tjmontufar@uth.hn>
2024-07-23 13:02:02 -0600

    Creando puntos de patrullaje (Trabajo en Casa: 23/07/2024) 1:01 pm

--- enemy.py
+++ enemy.py
@@ -1,14 +1,51 @@
 import pygame as pg
+from pygame.math import Vector2
+import math
 
 class Enemy(pg.sprite.Sprite):
-    def __init__(self,pos,image):
+    def __init__(self, waypoints, image):
         pg.sprite.Sprite.__init__(self)
-        self.image = image
+        self.waypoints = waypoints
+        self.pos = Vector2(self.waypoints[0])
+        self.target_waypoint = 1
+        self.speed = 2
+        self.angle = 0
+        self.original_image = image
+        self.image = pg.transform.rotate(self.original_image, self.angle)
         self.rect = self.image.get_rect()
-        self.rect.center = pos
+        self.rect.center = self.pos
 
     def update(self):
         self.move()
+        self.rotate()
 
     def move(self):
-        self.rect.x += 1
+        # Calcular la dirección hacia el siguiente waypoint
+        if self.target_waypoint < len(self.waypoints):
+            self.target = Vector2(self.waypoints[self.target_waypoint])
+            self.movement = self.target - self.pos
+        else:
+            # Si no hay más waypoints, el enemigo se detiene
+            self.kill()
+
+        # Calcular la distancia hacia el objetivo
+        dist = self.movement.length()
+        # Ajustar la velocidad si es necesario para no pasar el waypoint
+        if dist >= self.speed:
+            self.pos += self.movement.normalize() * self.speed
+        else:
+            if dist != 0:
+                self.pos += self.movement.normalize() * dist
+            self.target_waypoint += 1
+
+    def rotate(self):
+        # Calcular la dirección hacia el siguiente waypoint
+        dist = self.target - self.pos
+
+        # Usar la distancia para calcular el ángulo
+        self.angle = math.degrees(math.atan2(-dist[1], dist[0]))
+
+        # Rotar la imagen y actualizar el rectángulo
+        self.image = pg.transform.rotate(self.original_image, self.angle)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.pos
--- main.py
+++ main.py
@@ -14,7 +14,14 @@ enemy_image = pg.image.load("imagen/enemy_1.png").convert_alpha()
 # Crear grupos de imagenes
 enemy_group = pg.sprite.Group()
 
-enemy = Enemy((200,300),enemy_image)
+waypoints = [
+    (100,100),
+    (400,200),
+    (400,100),
+    (200,300),
+]
+
+enemy = Enemy(waypoints,enemy_image)
 
 # Añadir imagenes a los grupos
 enemy_group.add(enemy)
@@ -25,6 +32,9 @@ while run:
     clock.tick(c.FPS)
     # Colocar un color de fondo
     screen.fill("grey100")
+
+    # Dibujar el camino del enemigo
+    pg.draw.lines(screen, "grey0", False, waypoints)
 
     # Actualizar los grupos
     enemy_group.update()
