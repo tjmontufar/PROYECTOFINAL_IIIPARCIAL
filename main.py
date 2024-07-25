52a7f9b0f4ae9925df91033fdad6402e2be94281
tjmontufar <tjmontufar@uth.hn>
2024-07-24 23:06:42 -0600

    Corrigiendo errores (Trabajo en casa: 24/07/2024) 11:06 pm

--- main.py
+++ main.py
@@ -18,34 +18,35 @@ pg.display.set_caption("Tower Defence")
 
 #Variables del Juego
 placing_turrets = False
+selected_turret = None
 
 # Cargar imagenes
 
 # Mapa
-map_image = pg.image.load("niveles/level.png").convert_alpha()
+map_image = pg.image.load("imagen/niveles/level.png").convert_alpha()
 
 # Enemigo
-enemy_image = pg.image.load("imagen/enemy_1.png").convert_alpha()
+enemy_image = pg.image.load("imagen/enemigos/enemy_1.png").convert_alpha()
 
 # Botones
-buy_turret_image = pg.image.load("imagen/buy_turret.png").convert_alpha()
-cancel_image = pg.image.load("imagen/cancel.png").convert_alpha()
+buy_turret_image = pg.image.load("imagen/botones/buy_turret.png").convert_alpha()
+cancel_image = pg.image.load("imagen/botones/cancel.png").convert_alpha()
 
 #Torre individual imagen 
-turret_sheet = pg.image.load("imagen/turret_1.png").convert_alpha()
-cursor_turret = pg.image.load("imagen/cursor_turret.png").convert_alpha()
+turret_sheet = pg.image.load("imagen/torretas/turret_1.png").convert_alpha()
+cursor_turret = pg.image.load("imagen/torretas/cursor_turret.png").convert_alpha()
 
 # Cargar el archivo json para la ruta del nivel
-with open("niveles/level.tmj") as file:
+with open("imagen/niveles/level.tmj") as file:
     world_data = json.load(file)
 
 # Crear un grupo de torretas
 def create_turret(mouse_pos):
-    mouse_title_x = mouse_pos[0] // c.TILE_SIZE
-    mouse_title_y = mouse_pos[1] // c.TILE_SIZE
+    mouse_tile_x = mouse_pos[0] // c.TILE_SIZE
+    mouse_tile_y = mouse_pos[1] // c.TILE_SIZE
 
     #Calcular la secuencia de numeros del titulo
-    mouse_tile_num = (mouse_title_y * c.COLS) + mouse_title_x
+    mouse_tile_num = (mouse_tile_y * c.COLS) + mouse_tile_x
 
     #Chequiar si el titulo es valido
     if world.tile_map[mouse_tile_num] == 7:
@@ -53,13 +54,24 @@ def create_turret(mouse_pos):
         #Chequiar si el cursor se encuentra sobre una torre
         space_is_free = True
         for turret in turret_group:
-            if (mouse_title_x, mouse_title_y) == (turret.title_x, turret.title_y):
+            if (mouse_tile_x, mouse_tile_y) == (turret.tile_x, turret.tile_y):
                 space_is_free = False
 
         #Si la torre no es una torre no valida, no crearla
         if space_is_free == True:
-            new_turret = Turret(turret_sheet, mouse_title_x, mouse_title_y)
+            new_turret = Turret(turret_sheet, mouse_tile_x, mouse_tile_y)
             turret_group.add(new_turret)
+
+def select_turret(mouse_pos):
+    mouse_tile_x = mouse_pos[0] // c.TILE_SIZE
+    mouse_tile_y = mouse_pos[1] // c.TILE_SIZE
+    for turret in turret_group:
+        if (mouse_tile_x, mouse_tile_y) == (turret.tile_x, turret.tile_y):
+            return turret
+
+def clear_selection():
+    for turret in turret_group:
+        turret.selected = False
 
 # Crear el mundo
 world = World(world_data, map_image)
@@ -92,6 +104,11 @@ while run:
 
     # Actualizar los grupos
     enemy_group.update()
+    turret_group.update()
+
+    # Marcar la torre seleccionada
+    if selected_turret:
+        selected_turret.selected = True
 
     #############################
     # Seccion de Dibujo
@@ -99,9 +116,11 @@ while run:
 
     # Dibujar el mundo
     world.draw(screen)
+
     # Dibujar grupos
     enemy_group.draw(screen)
-    turret_group.draw(screen)
+    for turret in turret_group:
+        turret.draw(screen)
 
     # Dibujar botones
     if turret_buy_button.draw(screen):
@@ -128,8 +147,13 @@ while run:
             mouse_pos = pg.mouse.get_pos()
             #Chequiar si el ratón se encuentra sobre una torre
             if mouse_pos[0] < c.SCREEN_WIDTH and mouse_pos[1] < c.SCREEN_HEIGHT:
+                # Limpiar la seleccion de la torre
+                selected_turret = None
+                clear_selection()
                 if placing_turrets == True:
                     create_turret(mouse_pos)
+                else:
+                    selected_turret = select_turret(mouse_pos)
 
 
     pg.display.flip()
--- turret.py
+++ turret.py
@@ -2,19 +2,20 @@ import pygame as pg
 import constants as c
 
 class Turret(pg.sprite.Sprite):
-    def __init__(self, sprite_sheet, title_x, title_y):
+    def __init__(self, sprite_sheet, tile_x, tile_y):
         pg.sprite.Sprite.__init__(self)
         self.range = 90
         self.cooldown = 1500
         self.last_shot = pg.time.get_ticks()
+        self.selected = False
 
         # Posicion de la variable
-        self.title_x = title_x
-        self.title_y = title_y
+        self.tile_x = tile_x
+        self.tile_y = tile_y
 
         # Calcular el centro de las coordenadas
-        self.x = (self.title_x + 0.5)* c.TILE_SIZE
-        self.y = (self.title_y + 0.5)* c.TILE_SIZE
+        self.x = (self.tile_x + 0.5)* c.TILE_SIZE
+        self.y = (self.tile_y + 0.5)* c.TILE_SIZE
 
         # Animacion de la variable
         self.sprite_sheet = sprite_sheet
@@ -26,6 +27,15 @@ class Turret(pg.sprite.Sprite):
         self.image = self.animation_list[self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = (self.x , self.y)
+
+        # Crear un rango de torres
+        self.range_image = pg.Surface((self.range * 2, self.range * 2))
+        self.range_image.fill((0, 0, 0))
+        self.range_image.set_colorkey((0, 0, 0))
+        pg.draw.circle(self.range_image, "grey100", (self.range, self.range), self.range)
+        self.range_image.set_alpha(100)
+        self.range_rect = self.range_image.get_rect()
+        self.range_rect.center = self.rect.center
         
     def load_images(self):
         # Extraer las imagenes de la hoja de animacion
@@ -56,3 +66,9 @@ class Turret(pg.sprite.Sprite):
                 self.frame_index = 0
                 # Recorrer el tiempo completo y limpieza de torres
                 self.last_shot = pg.time.get_ticks()
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+        if self.selected:
+            surface.blit(self.range_image, self.range_rect)
